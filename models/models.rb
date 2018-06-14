162997f90aca75a32ab868b1ed3e4f37dca7727e
MayaCoro <mayascoro@gmail.com>
2018-06-14 18:25:04 +0000

    automatically backed up by learn

---
+++ models/models.rb
@@ -0,0 +1,7 @@
+# def get_fortune()
+#   fortunes_array = ["You'll have a great day", "Someone will give you a dirty look", "You will soon meet the love of your life", "Financial problems ahead", "Birds will attack you", "Someone you love will give you a wonderful gift", "Someone you love will become terribly ill",
+#     ]
+#     return fortunes_array.sample
+# end
+
+# puts get_fortune
